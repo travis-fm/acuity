7e6cb50b39eedfb458fa4df65c2a044a9a1aed40
Travis Gibson <1546593+travis-fm@users.noreply.github.com>
2025-07-09 11:32:39 -0400

    Add UI code (#1)
    * Add initial UI framework
    
    * test adding layout
    
    * Add prototype UI structure for sensors and modules
    
    * Add initial UI framework
    
    * test adding layout
    
    * Add prototype UI structure for sensors and modules
    
    * Merge branch 'add-ui' of github.com:travis-fm/acumen into add-ui
    
    * Add additional UI for modules
    
    * Additional block UI code
    
    * More module UI
    
    * Edit index
    
    * Edit index again
    
    * Change sensor constraint
    
    * Additional UI updates and update sensor values
    
    * Forgot module_block.render()
    
    * Add sensor and app refresh intervals. Update event checks to avoid blocking.
    
    * Remove last_app_refresh. Will handle drawing more organically instead.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use glob::glob;
+use ratatui::layout::{Alignment, Constraint, Direction, Flex, Layout};
 use std::fs::read_to_string;
 use std::io::{self};
 use std::path::{Path, PathBuf};
 use std::thread::sleep;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal,
+    Frame
+};
 
 #[derive(PartialEq, Debug)]
 enum SensorType {
@@ -70,6 +83,24 @@ impl Sensor {
         }
     }
 }
+impl Widget for &Sensor {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let [name_area, value_area] = Layout::horizontal([
+            Constraint::Percentage(25),
+            Constraint::Percentage(75),
+        ]).areas(area);
+
+        let render_name = Line::from(self.display_name.as_str());
+        let render_curr_value = Line::from(self.value.to_string());
+
+        Paragraph::new(render_name)
+            .alignment(Alignment::Left)
+            .render(name_area, buf);
+        Paragraph::new(render_curr_value)
+            .alignment(Alignment::Right)
+            .render(value_area, buf);
+    }
+}
 
 impl HwMon {
     fn new(hwmon_path: PathBuf) -> io::Result<Self> {
@@ -130,15 +161,133 @@ impl HwMon {
         }
     }
 }
+impl Widget for &HwMon {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let module_name = Line::from(self.display_name.as_str());
+        let module_block = Block::bordered()
+            .border_set(border::PLAIN)
+            .title(module_name.centered());
+        let mut constraints = vec![];
+        self.sensors.iter().for_each(|_| constraints.push(Constraint::Fill(1)));
 
-fn main() {
-    let mut modules: Vec<HwMon> = vec![];
+        let layout = Layout::vertical(constraints).split(module_block.inner(area));
 
+        module_block.render(area, buf);
+        for i in 0..self.sensors.len() {
+            self.sensors[i].render(layout[i], buf);
+        }
+    }
+}
+
+#[derive(Debug)]
+struct App {
+    exit: bool,
+    modules: Vec<HwMon>,
+    sensor_refresh_interval: Duration,
+    app_frame_rate: Duration,
+    last_sensor_refresh: Instant,
+}
+
+impl App {
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            self.update_modules();
+            terminal.draw(|f| self.draw(f))?;
+            self.handle_events()?;
+        }
+
+        Ok(())
+    }
+
+    fn update_modules(&mut self) {
+        if Instant::now() >= self.last_sensor_refresh + self.sensor_refresh_interval {
+            for module in &mut self.modules {
+                module.update_sensors();
+            }
+
+            self.last_sensor_refresh = Instant::now();
+        }
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        if event::poll(Duration::from_secs(0))? {
+            match event::read()? {
+                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                    self.handle_key_event(key_event)
+                }
+
+                _ => {}
+            };
+        }
+        
+        Ok(())
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let app_title = Line::from("Acumen Hardware Monitor");
+        let app_version = Line::from("v0.0.1-dev");
+        let app_block = Block::bordered()
+            .title(app_title.centered())
+            .title_bottom(app_version.right_aligned())
+            .border_set(border::THICK);
+
+        /*
+        let header_footer_size = 16;
+        let main_area_size = app_block.inner(area).height - (header_footer_size * 2);
+        let [header_area, main_area, footer_area] = Layout::vertical([
+            Constraint::Max(header_footer_size),
+            Constraint::Length(main_area_size),
+            Constraint::Max(header_footer_size),
+        ]).areas(app_block.inner(area));
+        */
+        let [main_area] = Layout::vertical([
+            Constraint::Fill(1)
+        ]).areas(app_block.inner(area));
+
+        let module_col_size = 100 / if self.modules.len() > 0 { self.modules.len() } else { 1 };
+        let module_cols = (0..self.modules.len())
+            .map(|_| Constraint::Percentage(module_col_size as u16));
+        let module_layout = Layout::horizontal(module_cols).spacing(1).split(main_area);
+
+        app_block.render(area, buf);
+
+        for i in 0..self.modules.len() {
+            self.modules[i].render(module_layout[i], buf);
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut app = App {
+        exit: false,
+        modules: vec![],
+        sensor_refresh_interval: Duration::from_millis(1000),
+        app_frame_rate: Duration::from_secs_f64(1.0/60.0),
+        last_sensor_refresh: Instant::now(),
+    };
+    
     match glob("/sys/class/hwmon/hwmon*") {
         Ok(paths) => {
             for path in paths.flatten() {
                 if let Ok(module) = HwMon::new(path) {
-                    modules.push(module);
+                    app.modules.push(module);
                 }
             }
         }
@@ -147,6 +296,12 @@ fn main() {
         }
     }
 
+    let mut terminal = ratatui::init();
+    let app_result = app.run(&mut terminal);
+    ratatui::restore();
+    app_result
+
+    /*
     loop {
         for module in &mut modules {
             module.update_sensors();
@@ -155,4 +310,5 @@ fn main() {
 
         sleep(Duration::from_secs(5));
     }
+    */
 }
